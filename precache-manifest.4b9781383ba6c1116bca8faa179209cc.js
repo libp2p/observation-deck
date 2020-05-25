bfd55cbc1da3bb84cde7ceb2381bbd979f8db20e
AlanSl <AlanSl@users.noreply.github.com>
2020-05-25 10:47:09 +0000

    Updates

---
+++ precache-manifest.4b9781383ba6c1116bca8faa179209cc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedb8218010065316383c30e853ea707",
+    "url": "/nearform/libp2p-observation-deck/index.html"
+  },
+  {
+    "revision": "1c091d7a2cfc863ff27f",
+    "url": "/nearform/libp2p-observation-deck/static/js/2.a6636292.chunk.js"
+  },
+  {
+    "revision": "1d83dd4543b4df82eaf7",
+    "url": "/nearform/libp2p-observation-deck/static/js/main.962f9bcf.chunk.js"
+  },
+  {
+    "revision": "4d7f86d8d935a9c015ad",
+    "url": "/nearform/libp2p-observation-deck/static/js/runtime-main.c75460e5.js"
+  },
+  {
+    "revision": "f9679767487b4907fff1d91d6f81ebd9",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexMono-Light.f9679767.otf"
+  },
+  {
+    "revision": "dbf7ec0bee826375ea26a43d37615cba",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexMono-SemiBold.dbf7ec0b.otf"
+  },
+  {
+    "revision": "3ca905f2d84214ff5a71cdf3b119682b",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexMono-Text.3ca905f2.otf"
+  },
+  {
+    "revision": "b7601880aa2b7c399d673c66e3777d1b",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexSans-Bold.b7601880.otf"
+  },
+  {
+    "revision": "2c56095395245c63b14a3ddb012ab298",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexSans-ExtraLight.2c560953.otf"
+  },
+  {
+    "revision": "074e5648744dcc5fd2c4d000e2e2ff1a",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexSans-Light.074e5648.otf"
+  },
+  {
+    "revision": "749823864c923056a30ec5c89bb40119",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexSans-Medium.74982386.otf"
+  },
+  {
+    "revision": "177a43ac4fc0a37d2a513f485415df99",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexSans-Regular.177a43ac.otf"
+  },
+  {
+    "revision": "1f4b8be3cd1279667d74469b65fc2bd4",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexSans-SemiBold.1f4b8be3.otf"
+  },
+  {
+    "revision": "9ac3a2a33031814989c494d0b2db2027",
+    "url": "/nearform/libp2p-observation-deck/static/media/IBMPlexSans-Text.9ac3a2a3.otf"
+  },
+  {
+    "revision": "254370afdfd4b4a1c376844a1feb4b61",
+    "url": "/nearform/libp2p-observation-deck/static/media/Nexa-Bold.254370af.otf"
+  },
+  {
+    "revision": "5c6f5c998df9b3e3b7e04ed8557450cb",
+    "url": "/nearform/libp2p-observation-deck/static/media/Nexa-Light.5c6f5c99.otf"
+  },
+  {
+    "revision": "7e73cafb298429b165519992e0799a86",
+    "url": "/nearform/libp2p-observation-deck/static/media/sample-1min.7e73cafb.png"
+  },
+  {
+    "revision": "89e9e4bda80afaca0f6781d6af0b403d",
+    "url": "/nearform/libp2p-observation-deck/static/media/sample-1min.89e9e4bd.mock"
+  },
+  {
+    "revision": "1041f34551eb39051f98decbc2e53412",
+    "url": "/nearform/libp2p-observation-deck/static/media/sample-2min.1041f345.png"
+  },
+  {
+    "revision": "9d7b38f94aec1dae6e3567d8c53cc6e6",
+    "url": "/nearform/libp2p-observation-deck/static/media/sample-2min.9d7b38f9.mock"
+  },
+  {
+    "revision": "e30f2a1b5b4596c77d24ad1c377ed11c",
+    "url": "/nearform/libp2p-observation-deck/static/media/sample-3min.e30f2a1b.png"
+  },
+  {
+    "revision": "f1d182fa87c93a4839a2781d46ab7ae6",
+    "url": "/nearform/libp2p-observation-deck/static/media/sample-3min.f1d182fa.mock"
+  },
+  {
+    "revision": "3a5301f87ed750dd69aac1cd8fab13d3",
+    "url": "/nearform/libp2p-observation-deck/static/media/screenshot.3a5301f8.png"
+  },
+  {
+    "revision": "e1a6ab626f9ce0eb36be7d3511cfe6b5",
+    "url": "/nearform/libp2p-observation-deck/static/media/screenshot.e1a6ab62.png"
+  }
+]);
